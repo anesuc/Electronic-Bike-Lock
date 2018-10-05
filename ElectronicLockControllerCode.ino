5dd04bc25301a3e566d2b0f2d1d10d4302739ec0
anesuc <17863266+anesuc@users.noreply.github.com>
2018-10-05 17:57:02 +1000

    Removed other unverified pin layouts
    Removed other pin layouts even though they should work, they are unrelated to this specific Arduino board and it's better to have verified information only.

--- ElectronicLockControllerCode.ino
+++ ElectronicLockControllerCode.ino
@@ -7,17 +7,17 @@
  * By Group 37 Engineering Design 3B
  * 
  * 
- * Typical pin layout used:
- * -----------------------------------------------------------------------------------------
- *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
- *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
- * Signal      Pin          Pin           Pin       Pin        Pin              Pin
- * -----------------------------------------------------------------------------------------
- * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
- * SPI SS      SDA(SS)      10            53        D10        10               10
- * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
- * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
- * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+ * Pin layout used:
+ * -------------------------------------
+ *             MFRC522      Arduino     
+ *             Reader/PCD   Uno/101     
+ * Signal      Pin          Pin         
+ * -------------------------------------
+ * RST/Reset   RST          9           
+ * SPI SS      SDA(SS)      10           
+ * SPI MOSI    MOSI         11 / ICSP-4   
+ * SPI MISO    MISO         12 / ICSP-1   
+ * SPI SCK     SCK          13 / ICSP-3   
  */
 
 #include <SPI.h>
